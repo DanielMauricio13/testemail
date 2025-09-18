2a3ea0dd0921ca57f0049ad97b49f22deaea1276
Tyler Mestery <tyler@mestery.com>
2025-09-18 15:55:10 -0500

    Created the complete skeleton for the settings page, including naviagtion.

--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/SettingsActivity.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/SettingsActivity.java
@@ -8,10 +8,34 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
+    private Button homeButton;  // define profile button variable
+    private Button profileButton;  // define settings button variable
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profiles);
+        setContentView(R.layout.activity_settings);
+
+        homeButton = findViewById(R.id.stngs_home_btn);
+        profileButton = findViewById(R.id.stngs_prfile_btn);
+
+        // Click listener on home button pressed:
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on profile button pressed:
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, ProfilesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
