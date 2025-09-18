8b80c1ad448862e0428929a14a97622d5b1d8310
Tyler Mestery <tyler@mestery.com>
2025-09-18 10:08:43 -0500

    Added more buttons to the main page for better navigation. Completed main page skeleton.

--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/MainActivity.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/MainActivity.java
@@ -1,43 +1,11 @@
 package com.example.cymarket;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-/*
-
-1. To run this project, open the directory "Android Example", otherwise it may not recognize the file structure properly
-
-2. Ensure you are using a compatible version of gradle, to do so you need to check 2 files.
-
-    AndroidExample/Gradle Scripts/build.gradle
-    Here, you will have this block of code. Ensure it is set to a compatible version,
-    in this case 8.12.2 should be sufficient:
-        plugins {
-            id 'com.android.application' version '8.12.2' apply false
-        }
-
-    Gradle Scripts/gradle-wrapper.properties
-
-3. This file is what actually determines the Gradle version used, 8.13 should be sufficient.
-    "distributionUrl=https\://services.gradle.org/distributions/gradle-8.13-bin.zip" ---Edit the version if needed
-
-4. You might be instructed by the plugin manager to upgrade plugins, accept it and you may execute the default selected options.
-
-5. Press "Sync project with gradle files" located at the top right of Android Studio,
-   once this is complete you will be able to run the app
-
-   This version is compatible with both JDK 17 and 21. The Java version you want to use can be
-   altered in Android Studio->Settings->Build, Execution, Deployment->Build Tools->Gradle
-
- */
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,19 +13,28 @@ public class MainActivity extends AppCompatActivity {
     private TextView usernameText;  // define username textview variable
     private Button loginButton;     // define login button variable
     private Button signupButton;    // define signup button variable
+    private Button profileButton;  // define profile button variable
+    private Button messagesButton;  // define messages button variable
+    private Button settingsButton;  // define settings button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);             // link to Main activity XML
 
-        /* initialize UI elements */
+        // Initialize UI elements:
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
-        loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
-        signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
 
-        /* extract data passed into this activity from another activity */
+        // Link all the buttons:
+        loginButton = findViewById(R.id.main_login_btn);
+        signupButton = findViewById(R.id.main_signup_btn);
+        profileButton = findViewById(R.id.main_profiles_btn);
+        messagesButton = findViewById(R.id.main_messages_btn);
+        settingsButton = findViewById(R.id.main_settings_btn);
+
+
+        // Extract data passed into this activity from another activity:
         Bundle extras = getIntent().getExtras();
         if(extras == null) {
             messageText.setText("Home Page");
@@ -69,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
         }
 
-        /* click listener on login button pressed */
+        // Click listener on login button pressed:
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,13 +57,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /* click listener on signup button pressed */
+        // Click listener on signup button pressed:
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 /* when signup button is pressed, use intent to switch to Signup Activity */
                 Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on profiles button pressed:
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(MainActivity.this, ProfilesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on messages button pressed:
+        messagesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(MainActivity.this, MessagesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on settings button pressed:
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
                 startActivity(intent);
             }
         });
--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/ProfilesActivity.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/ProfilesActivity.java
@@ -1,9 +1,39 @@
 package com.example.cymarket;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfilesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiles);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
 }
