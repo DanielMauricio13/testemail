7817dbe8f86fba760bc311de633c301bff0819ac
Tyler Mestery <tyler@mestery.com>
2025-09-18 11:53:44 -0500

    Completed the skeleton for the Messages page, can move between all of its connected screens.

--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/MessagesActivity.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/MessagesActivity.java
@@ -3,15 +3,53 @@ package com.example.cymarket;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MessagesActivity extends AppCompatActivity {
+
+    private Button homeButton;  // define profile button variable
+    private Button profileButton;  // define messages button variable
+    private Button sellButton;  // define settings button variable
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profiles);
+        setContentView(R.layout.activity_messages);
+
+        // Link all the buttons:
+        homeButton = findViewById(R.id.msgs_home_page_btn);
+        profileButton = findViewById(R.id.msgs_profile_btn);
+        sellButton = findViewById(R.id.msgs_sell_btn);
+
+        // Click listener on home button pressed:
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessagesActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on profile button pressed:
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessagesActivity.this, ProfilesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on sell button pressed:
+        sellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessagesActivity.this, SellActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
