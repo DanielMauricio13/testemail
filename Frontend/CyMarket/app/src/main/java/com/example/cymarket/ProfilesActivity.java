cdf73480189457b4295d6f36abd54df207bd44cf
Tyler Mestery <tyler@mestery.com>
2025-09-18 12:37:11 -0500

    Completed the skeleton for the profiles page.

--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/ProfilesActivity.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/ProfilesActivity.java
@@ -1,14 +1,54 @@
 package com.example.cymarket;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfilesActivity extends AppCompatActivity {
+
+    private Button homeButton;  // define profile button variable
+    private Button messagesButton;  // define messages button variable
+    private Button settingsButton;  // define settings button variable
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiles);
+
+        // Link all the buttons:
+        homeButton = findViewById(R.id.prfls_home_page_btn);
+        messagesButton = findViewById(R.id.prfls_messages_btn);
+        settingsButton = findViewById(R.id.prfls_setting_btn);
+
+        // Click listener on home button pressed:
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilesActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on messages button pressed:
+        messagesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilesActivity.this, MessagesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on settings button pressed:
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilesActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
