9e25d591129bdc8aed31cf3ea7ce2d9eff6b2de2
Tyler Mestery <tyler@mestery.com>
2025-09-19 23:18:53 -0500

    Created a simple splash screen that displays a app logo/slogan. Added logo to project folder.

---
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.cymarket;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen); // Changed to activity_splash layout
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
