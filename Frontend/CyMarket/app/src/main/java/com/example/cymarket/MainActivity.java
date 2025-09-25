a0eb3f69baca0a69bac852347639607fc03a0781
Tyler Mestery <tyler@mestery.com>
2025-09-25 11:59:34 -0500

    Added a lot of elemenets to UI on home page, also added new art to resemble settings and profile buttons.

--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/MainActivity.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/MainActivity.java
@@ -7,57 +7,48 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginButton;     // define login button variable
-    private Button signupButton;    // define signup button variable
-    private Button profileButton;  // define profile button variable
+    private Button buyButton;     // define login button variable
+    private Button sellButton;  // define profile button variable
     private Button messagesButton;  // define messages button variable
-    private Button settingsButton;  // define settings button variable
+    private Button listingsButton;  // define settings button variable
+    private TextView profileText; // define profile text button
+    private TextView settingsText; // define profile text button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);             // link to Main activity XML
+        setContentView(R.layout.activity_main); // link to Main activity XML
 
         // Initialize UI elements:
-
-        // Link all the buttons:
-        loginButton = findViewById(R.id.main_login_btn);
-        signupButton = findViewById(R.id.main_signup_btn);
-        profileButton = findViewById(R.id.main_profiles_btn);
+        buyButton = findViewById(R.id.main_buy_btn);
+        sellButton = findViewById(R.id.main_sell_btn);
         messagesButton = findViewById(R.id.main_messages_btn);
-        settingsButton = findViewById(R.id.main_settings_btn);
+        listingsButton = findViewById(R.id.main_listings_btn);
+        profileText = findViewById(R.id.main_profile_txt);
+        settingsText = findViewById(R.id.main_settings_txt);
 
         // Click listener on login button pressed:
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        buyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 /* when login button is pressed, use intent to switch to Login Activity */
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Click listener on signup button pressed:
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                /* when signup button is pressed, use intent to switch to Signup Activity */
-                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                Intent intent = new Intent(MainActivity.this, BuyActivity.class);
                 startActivity(intent);
             }
         });
 
         // Click listener on profiles button pressed:
-        profileButton.setOnClickListener(new View.OnClickListener() {
+        sellButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 /* when signup button is pressed, use intent to switch to Signup Activity */
-                Intent intent = new Intent(MainActivity.this, ProfilesActivity.class);
+                Intent intent = new Intent(MainActivity.this, SellActivity.class);
                 startActivity(intent);
             }
         });
@@ -74,7 +65,29 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Click listener on settings button pressed:
-        settingsButton.setOnClickListener(new View.OnClickListener() {
+        listingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(MainActivity.this, ListingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on profile text button pressed:
+        profileText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(MainActivity.this, ProfilesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener on settings text button pressed:
+        settingsText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
