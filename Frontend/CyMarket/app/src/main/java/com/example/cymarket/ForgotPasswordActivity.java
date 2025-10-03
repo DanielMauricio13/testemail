057a00081033a5682d5bbf3b8305e4fddb7dcc54
Tyler Mestery <tyler@mestery.com>
2025-10-03 10:43:24 -0500

    Added screens and activities for 'forgot password'.

---
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.cymarket;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+
+}
