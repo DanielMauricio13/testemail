48e345b04ba533375e08a62af5e929d55966a58d
Tyler Mestery <tyler@mestery.com>
2025-10-02 19:16:34 -0500

    Working on getting POST set up with PFP image, doesn't quite work yet. Saved PFP locally for the time being.

--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/LoginActivity.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/LoginActivity.java
@@ -7,14 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.example.cymarket.R;
-
 import org.json.JSONException;
-
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
